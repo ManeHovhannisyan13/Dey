d8464a68ae6f66e180555ed719ac6f8f2f294946
Vero <Vero@WIN-JV3HDVOJ71D>
2024-01-19 15:31:32 +0400

    Добавьте файлы проекта.

---
+++ vv/FutureDay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureDay
+{
+    public class FutureDay
+    {
+        public static void DisplayFutureDay(int today, int daysAfter)
+        {
+            string[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            int futureDayIndex = (today + daysAfter) % 7;
+            string todayName = daysOfWeek[today];
+            string futureDayName = daysOfWeek[futureDayIndex];
+
+            Console.WriteLine($"Today is {todayName} and the next day is {futureDayName}");
+        }
+    }
+}
---
+++ vv/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureDay
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter today's day of the week (Sunday is 0, Monday is 1, ..., Saturday is 6): ");
+            int today = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of days after today: ");
+            int daysAfter = int.Parse(Console.ReadLine());
+
+            FutureDay.DisplayFutureDay(today, daysAfter); 
+        }
+    }
+}
